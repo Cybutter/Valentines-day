f2e5de937a4bd4479e884b50a2eebe5fa0f3558c
Butter-byte <hrishi18ganguli07@gmail.com>
2026-02-13 21:08:45 +0530

    removed temporary inits

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('highlight-btn');
-const text = document.getElementById('hello-text');
-
-button.addEventListener('click', () => {
-    text.classList.toggle('highlight');
-});
