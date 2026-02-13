b38098a8872282fe4c95dbf9765b5f9ce580fd47
Butter-byte <hrishi18ganguli07@gmail.com>
2026-02-13 20:59:35 +0530

    fixed script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById('highlight-btn');
-const text = document.getElementById('hello-test');
+const text = document.getElementById('hello-text');
 
 button.addEventListener('click', () => {
     text.classList.toggle('highlight');
